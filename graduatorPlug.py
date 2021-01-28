bfa4a267af81d51ba15a2c92007cec653465885c
dilanuslan <32961512+dilanuslan@users.noreply.github.com>
2021-01-29 02:46:46 +0300

    Update graduatorPlug.py

--- graduatorPlug.py
+++ graduatorPlug.py
@@ -9,14 +9,6 @@ import os #operating system support
 import re #regular expressions 
 
 import requests #this library allows to make requests like get, post, etc. 
-
-from contextlib import closing #returns a context manager that closes a page upon completion of the block
-from tempfile import NamedTemporaryFile #the file is guaranteed to have a visible name in the file system
-
-from beets.util.artresizer import ArtResizer #artresizer also belongs to beets and it is used as a helper for this project
-
-from beets.util import bytestring_path #this is used for setting the file paths  
-
 from bs4 import BeautifulSoup #BeautifulSoup will be used for fetching lyrics
 
 import warnings #for warning control
@@ -28,16 +20,6 @@ import musicbrainzngs #this library is used to reach Musicbrainz API directly to
 from wordcloud import WordCloud #used for creating wordclouds of albums
 
 import unittest
-
-
-class Image(object):
-
-    def __init__(self, log, path=None, url=None, match=None, size=None): #constructor for candidate image
-        self._log = log #for beetlogs.txt
-        self.path = path #path of the image
-        self.url = url #url of the image
-        self.match = match #result of checking 
-        self.size = size #size of the image
 
 
 #args and kwargs are used to pass an argument list to a function
@@ -86,50 +68,16 @@ class CoverArtSource(object):
         self._config = config
         self.match_by = match_by  
 
-    def _candidate(self, **kwargs):
-        return Image(log=self._log, **kwargs) 
-
     def request(self, *args, **kwargs):
         return getLog(self._log, *args, **kwargs)
 
 
 #The knowledge from this website is basically used in this part of the code: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
 
-    def get_image(self, candidate, plugin): #Downloads an image from an URL and checks whether it is an image or not. If so, returns a path to the downloaded image. Otherwise, returns None.
-      
-        if (plugin.maxwidth):
-            candidate.url = ArtResizer.shared.proxy_url(plugin.maxwidth, candidate.url) #Modifies an image URL according the method, returning a new URL. For WEBPROXY, a URL on the proxy server is returned. Otherwise, the URL is returned unmodified.
-
-        with closing(self.request(candidate.url, stream=True)) as response:
-            content_type = response.headers.get('Content-Type', None) #content type should be either jpg or png
-
-            #Download the image to a temporary file. 
-            data = response.iter_content(chunk_size=1024) #Iterates over the response data. When stream=True is set on the request, this avoids reading the content at once into memory for large responses. The chunk size is the number of bytes it should read into memory. This is not necessarily the length of each item returned as decoding can take place. 
-            header = b'' 
-            for d in data:
-                header += d
-                if (len(header) >= 32): #we can read up to 32 bytes
-                    break
-                else: 
-                    return
-
-            if (content_type not in IMAGE_TYPES): #if the content type is not jpg or png, it is logged
-                self._log.debug('not a supported image: {}', content_type or 'unknown content type')
-                return
-
-            extension = b'.' + IMAGE_TYPES[content_type][0] #extension will be either .jpg or .png
-
-            with NamedTemporaryFile(suffix=extension, delete=False) as file:
-                file.write(header)  #write the first already loaded part of the image
-                for i in data:  #download the remaining part of the image
-                    file.write(i)
-            self._log.debug('downloaded art to: {0}', util.displayable_path(file.name)) #logging the download operation
-            candidate.path = util.bytestring_path(file.name) #path of the candidate image is assigned using the bytestring_path function
-            return
-
 
 class CoverArtArchive(CoverArtSource): #this is the main website used in the project to get images 
-    MATCHING_CRITERIA = ['release'] 
+
+    Type = ['release'] 
 
     if (util.SNI_SUPPORTED): #server name indication
         URL = 'https://coverartarchive.org/release/{mbid}/front' #mbid = MusicBrainz ID
@@ -138,8 +86,27 @@ class CoverArtArchive(CoverArtSource): #this is the main website used in the pro
 
     def get(self, album, plugin, paths): 
         if (album.mb_albumid): #Return the Cover Art Archive URLs using album MusicBrainz release ID.
-            yield self._candidate(url=self.URL.format(mbid=album.mb_albumid))
+            save_path = '/Users/dilanuslan/Desktop/NewMusic/'  #the constant part of the path
+            save_path = save_path + album.albumartist + '/' + album.album #the artist name and the album name is added to the path 
+            filename = "cover.jpg"  
 
+            finalname = os.path.join(save_path, filename) #adding filename to the path
+
+            pic_url = self.URL.format(mbid=album.mb_albumid)
+
+            with open(finalname, 'wb') as cover:
+                response = requests.get(pic_url, stream=True)
+
+                if not response.ok:
+                    os.remove(finalname)
+
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+
+                    cover.write(block)
+
+        return save_path
 
 
 SOURCE = ['coverart'] #album covers are taken from coverartarchive.org
@@ -155,6 +122,7 @@ IMAGE_TYPES = { #the retrieved images should be in these formats
 
 
 class Lyric(object): #general class for lyrics
+
     def __init__(self, config, log):
         self._log = log
 
@@ -183,7 +151,7 @@ def slugify(text):
 
 class Genius(Lyric): #deriving genius class from lyric class
 
-    base_url = "https://api.genius.com" #our base url is the api for genius
+    url = "https://api.genius.com" #our base url is the api for genius
 
     def __init__(self, config, log):
         super(Genius, self).__init__(config, log)
@@ -209,28 +177,22 @@ class Genius(Lyric): #deriving genius class from lyric class
             return None
 
         for hit in json["response"]["hits"]: #assigning the artist name 
-            hit_artist = hit["result"]["primary_artist"]["name"]
+            artist_hit = hit["result"]["primary_artist"]["name"]
 
-            if (slugify(hit_artist) == slugify(artist)): #if slugified artist names are equal we scrape lyrics
+            if (slugify(artist_hit) == slugify(artist)): #if slugified artist names are equal we scrape lyrics
                 return self.scrapelyrics(self.get_url(hit["result"]["url"])) 
 
-        self._log.debug('No matching artist \'{0}\'', artist)
+        self._log.debug('No matching artist {0}', artist)
 
 
     def search(self, artist, title):
 
-        search_url = self.base_url + "/search" #obtained: https://api.genius.com/search
+        search_url = self.url + "/search" #obtained: https://api.genius.com/search
         data = {'q': title + " " + artist.lower()} #data is our query statement
-        try:
-            response = requests.get(search_url, data=data, headers=self.headers) #we try to get a response from this query (with artist name, title and specified headers)
-        except requests.RequestException as exception: #if we can not get a response
-            self._log.debug('Genius API request failed: {0}', exception)
-            return None
+        response = requests.get(search_url, data=data, headers=self.headers) #we try to get a response from this query (with artist name, title and specified headers)
 
-        try: #try if we can return a valid json format
-            return response.json() 
-        except ValueError: #raise a value error if the response is not appropriate
-            return None
+        return response.json() 
+
 
     def scrapelyrics(self, html):
 
@@ -244,7 +206,7 @@ class Genius(Lyric): #deriving genius class from lyric class
             div2 = soup.find("div", class_=re.compile("Lyrics__Container")) #Compile a regular expression pattern into a regular expression object
             if (not div2): #if can not be found
                 if soup.find("div", class_=re.compile("LyricsPlaceholder__Message"), string="This song is an instrumental"): #if a placeholder statement is found
-                    self._log.debug('Detected instrumental') #instrumental song 
+                    self._log.debug('Thid is an instrumental song') #instrumental song 
                     return "[Instrumental]" #this would be stored as the lyrics 
                 else:
                     self._log.debug("Couldn't scrape the page..") #otherwise we can not scrape the page
@@ -283,6 +245,8 @@ def unescape(text):
     return out
 
 
+
+
                                                                         # PLUGIN #
 
 
@@ -294,19 +258,19 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
     }
     
     def __init__(self): #constructor
+
         super(graduatorPlug, self).__init__()
 
         self.maxwidth = self.config['maxwidth'].get(int) #it can be up to 2000 as defined in config.yaml
 
-        cover_name = self.config['cover_name'].as_str_seq() #one of these elements in this list: ['cover', 'front', 'art', 'album', 'folder']
-        self.cover_name = list(map(util.bytestring_path, cover_name)) #bytestring_path: Given a path, which is either a bytes or a unicode, returns a str path (ensuring that we never deal with Unicode pathnames).
+        self.cover_name = self.config['cover_name'].as_str_seq() #bytestring_path: Given a path, which is either a bytes or a unicode, returns a str path (ensuring that we never deal with Unicode pathnames).
 
         if (self.config['auto']): #importing our plugin
             self.import_stages = [self.graduatorPlug]
 
         available_source = list(SOURCE) #putting our source into a list
         
-        available_source = [(s, c) for s in available_source for c in ART_SOURCE[s].MATCHING_CRITERIA] #creating a list as [(CoverArtArchive, release)]
+        available_source = [(s, c) for s in available_source for c in ART_SOURCE[s].Type] #creating a list as [(CoverArtArchive, release)]
  
         self.source = [ART_SOURCE[s](self._log, self.config, match_by=[c]) for s, c in available_source]
 
@@ -318,8 +282,8 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
         self.config['genius_api_key'].redact = True
 
 
-
     def commands(self): #this function adds graduatorPlug to beets command list
+
         command = ui.Subcommand('graduatorPlug', help='help Dilan to graduate from ITU CE') # :)
         command.parser.add_option( #finding cover arts
             '-c', '--cover', dest='coverart',
@@ -348,6 +312,7 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
         )
 
         def func(lib, opts, args): #main functionalities of the plugin
+
             print("\n")
             print("Please run the command with one of the following options: ")
             print("-c or --cover for finding the album cover")
@@ -356,6 +321,7 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
             print("-p or --print for printing lyrics to command line")
             print("-w or --write for writing lyrics to a file and creating a word cloud")
             print("\n\n")
+         
 
             if(opts.coverart):
                 self.graduatorPlug(lib, lib.albums(ui.decargs(args))) 
@@ -365,6 +331,7 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
             for album in albums:
                 if(opts.allreleases):
                     self.allreleases(lib, album)
+                    print("\n")
 
             items = lib.items(ui.decargs(args)) #from database we reach out to items table
 
@@ -375,24 +342,23 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
                 
                 if (item.lyrics): #if the lyrics are found
                     if (opts.printlyrics): #if there is a -p or --print option
+                        title = item.artist + " - " + item.title
+                        title = ui.colorize('action', title)
+                        ui.print_(title)
                         ui.print_(item.lyrics) #print lyrics to console
                         ui.print_("\n") #print a space character after each song
                     if (opts.writetofile): #if there is a -w or --write option
                         self.writetofile(lib, item) #writing lyrics to file
-                        print("\n")
          
 
         command.func = func #assign our functionalities
         return [command] #our command is working now
 
     def albumcover(self, album, paths):  #Given an Album object, returns a path to downloaded art for the album (or None if no art is found).  
+    
         result = None      
         # URLs might be invalid at this point, or the image may not fulfill the requirements
-        for candidate in self.source[0].get(album, self, paths):
-            self.source[0].get_image(candidate, self)
-            result = candidate
-            self._log.debug(u'using image {0}'.format(util.displayable_path(result.path)))
-            break
+        result = self.source[0].get(album, self, paths)
 
         if (result):
             return result
@@ -403,16 +369,15 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
         
         for album in albums:
             if (album.artpath and os.path.isfile(album.artpath)):
-                message = ui.colorize('action', 'already has album art')
+                message = ui.colorize('action', 'already has cover art')
                 self._log.info('{0}: {1}', album, message)  #prints out to command line 
             else:
                 localpath = [album.path]
 
-                candidate = self.albumcover(album, localpath)
-                if (candidate): #if the album art is found
-                    album.set_art(candidate.path) #this is a built-in function from beets 
+                image = self.albumcover(album, localpath)
+                if (image): #if the album art is found
                     album.store() #storing the changes in the database 
-                    message = ui.colorize('text_success', 'found album art') #print in green
+                    message = ui.colorize('text_success', 'found cover art') #print in green
                 else:
                     message = ui.colorize('text_error', 'cover art not found') #print in red
                 self._log.info('{0}: {1}', album, message) #prints out to command line
@@ -442,9 +407,13 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
         item.store() #store item in the database
 
     def writetofile(self, lib, item):
+
         save_path = '/Users/dilanuslan/Desktop/NewMusic/'  #the constant part of the path
         save_path = save_path + item.albumartist + '/' + item.album #the artist name and the album name is added to the path 
         filename = item.title + ".txt"  #the name of the file will be the song name
+
+        message = "Writing {} to file.... Adding data into word cloud.....".format(item.title)
+        print(message)
 
         finalname = os.path.join(save_path, filename) #adding filename to the path
 
@@ -473,7 +442,7 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
 
         save_path2 = '/Users/dilanuslan/Desktop/NewMusic/wordclouds/' 
         filename2 = item.albumartist + ".png"
-        finalname2 = os.path.join(save_path2, filename2)   
+        finalname2 = os.path.join(save_path2, filename2)  
         word_cloud.to_file(finalname2)
         image = word_cloud.to_image()
 
@@ -481,13 +450,16 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
 
 
     def allreleases(self, lib, album):
+
         musicbrainzngs.set_useragent("beets.io", "0.1", "beets.io")
         URL = 'https://coverartarchive.org/release/'
 
         save_path = '/Users/dilanuslan/Desktop/NewMusic/'  #the constant part of the path
         save_path = save_path + album.albumartist + '/' + album.album #the artist name and the album name is added to the path 
 
-        print ("Checking all releases for {}").format(album.album)
+        message = "Checking all releases for {0}".format(album.album)
+
+        print (message)
 
         ##this block creates a list that contains the musicbrainz id's of all releases of an album 
 
@@ -531,9 +503,3 @@ class graduatorPlug(BeetsPlugin, CoverArtSource): #derived from BeetsPlugin and 
                         break
 
                     cover.write(block)
-
-
-
-
-    
-
